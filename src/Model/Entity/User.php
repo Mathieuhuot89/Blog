5488f766a73bea7c1c1b9763a299b4beedc8d845
Mathieu <huot.mathieuu@gmail.com>
2022-02-24 18:07:35 +0100

    model user #2

--- src/Model/Entity/User.php
+++ src/Model/Entity/User.php
@@ -4,5 +4,33 @@ namespace Blog\Model\Entity;
 
 class User
 {
+    protected $iduser;
+    protected $username;
+    protected $password;
+    protected $email;
     
+    public function getIduser(){
+        return $this->iduser;
+    }
+    public function getUsername(){
+        return $this->username;
+    }
+    public function getPassword(){
+        return $this->password;
+    }
+    public function getEmail(){
+        return $this->email;
+    }
+    public function setIduser($iduser){
+         $this->iduser = $iduser;
+    }
+    public function setUsername($username){
+         $this->username = $username;
+    }
+    public function setPassword($password){
+         $this->password = $password;
+    }
+    public function setEmail($email){
+         $this->email = $email;
+    }
 }
--- src/Model/Manager/UserManager.php
+++ src/Model/Manager/UserManager.php
@@ -1,8 +1,15 @@
 <?php
 
 namespace Blog\Model\Manager;
+use Blog\Core\PDOConnection;
 
 class UserManager
 {
-    
+    public function getUser($userid){
+        $pdo = PDOConnection::connect();
+        $query = $pdo->query("SELECT * FROM user");
+        $users = $query->fetchAll();
+        return $users;
+
+    }
 }
